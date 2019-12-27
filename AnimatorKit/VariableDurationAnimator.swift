511327c26b1256bf31d895ac05ce7c514490298b
Shane Whitehead <shanew@swann.com>
2019-12-28 10:37:27 +1100

    Update to variable animator to support alteraion of both the duration and progress

--- AnimatorKit/VariableDurationAnimator.swift
+++ AnimatorKit/VariableDurationAnimator.swift
@@ -9,32 +9,22 @@
 import Foundation
 
 open class VariableDurationAnimator: DurationAnimator {
-	
-	let estimatedDuration: TimeInterval
 
 	public override init(duration: TimeInterval, timingFunction: Curve? = nil, repeats: Bool = false, ticker: DurationTicker? = nil, then: AnimationCompleted? = nil) {
-		estimatedDuration = duration
 		super.init(duration: duration, timingFunction: timingFunction, repeats: repeats, ticker: ticker, then: then)
 	}
-
-	/*
-	This will adjust the overall duration of the animator
-	based on the estimated duration so that it matches the sepecified
-	progression.
 	
-	It will also adjust the running time so the animator matches the specified
-	progress
-	*/
-	public func adjustDuration(forProgress progress: Double) {
-		let diff = estimatedTime(at: progress) - duration
-		duration = estimatedDuration - diff
-		
-		let target = duration * progress
-		startedAt = Date().addingTimeInterval(-target)
-	}
-	
-	func estimatedTime(at point: Double) -> TimeInterval {
-		return estimatedDuration * point
+	public func adjust(duration: TimeInterval, progress: Double) {
+		let wasRunning = isRunning
+		stop()
+		// Set the desired duration
+		self.duration = duration
+		// Calculate the amount of time used
+		let used = duration * progress
+		// Adjust the the anchor time
+		startedAt = Date().addingTimeInterval(-used)
+		guard wasRunning else { return }
+		start()
 	}
 
 }
